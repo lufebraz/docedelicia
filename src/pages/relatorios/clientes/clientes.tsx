08bc1cb84b09dab70ac6a3cbb099466a4f3c6177
lufebraz <luizbraz98@gmail.com>
2021-05-27 23:35:05 -0300

    ativo-desativo

--- src/pages/relatorios/clientes/clientes.tsx
+++ src/pages/relatorios/clientes/clientes.tsx
@@ -3,11 +3,13 @@ import { RepositoryClient } from '../../../utils/RepositoryClient';
 import { GetServerSideProps } from 'next';
 import { heroku } from '../../../services/api';
 import { NavMenu1 } from '../../../components/NavBar';
+import Link from 'next/link';
 
-interface Clientes {
-  nome: string;
-  tCelular: string;
-  id: number;
+type Clientes = {
+  id: number,
+  nome: string,
+  tCelular: string,
+  ativo: number,
 }
 type HomeProps = {
   clientes: Clientes[];
@@ -21,6 +23,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
       nome: clientes.nome,
       tCelular: clientes.tCelular,
       id: clientes.id,
+      ativo: clientes.ativo
     }
   });
 
@@ -43,8 +46,15 @@ export default function ConsultarCliente({ clientes }: HomeProps) {
 
         <div className={styles.lista}>
           <ul>
-            {clientesList.map(clientes => {
-              return (<RepositoryClient key={clientes.id} pessoa={clientes} />)
+          {clientesList.map(clientes => {
+              return (
+              <div key={clientes.id} className={styles.repo}>
+                <strong>{clientes.nome}</strong>
+                <strong>{clientes.tCelular}</strong>
+                <strong>{clientes.ativo==1? 'ativo' : 'desativado'}</strong>
+                <Link href={`cliente/${clientes.id}`}>Visualizar</Link>
+              </div>
+              )
             })}
           </ul>
         </div>
--- src/pages/relatorios/produtos/produto/[slug].tsx
+++ src/pages/relatorios/produtos/produto/[slug].tsx
@@ -50,7 +50,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     fabricanteId: data.fabricanteId
   }
 
-  const res = await fetch(`http://docedelicia.ignorelist.com:8080/api/fabricante/ativos`)
+  const res = await fetch(`https://docedelicia.herokuapp.com/api/fabricante/ativos`)
   const data1 = await res.json()
 
   const fabricante = data1.map(fabricante => {
