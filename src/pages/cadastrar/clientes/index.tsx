c1005ea92ec50fb2df9ce4ef79a304d7c4743abb
lufebraz <luizbraz98@gmail.com>
2021-05-29 20:18:46 -0300

    some changes

--- src/pages/cadastrar/clientes/index.tsx
+++ src/pages/cadastrar/clientes/index.tsx
@@ -31,6 +31,14 @@ type Cliente = {
   ]
 }
 
+type Endereco = {
+  logradouro: string;
+  localidade: string;
+  bairro: string;
+  uf: string;
+}
+
+
 export default function Cadastrar() {
   const { register, handleSubmit } = useForm<Cliente>();
   const onSubmit = handleSubmit(async (values) => {
@@ -51,7 +59,6 @@ export default function Cadastrar() {
     })
     Router.push(`/consultar/clientes`)
   })
-
 
   const [tel, setTel] = useState('')
   useEffect(() => {
@@ -79,29 +86,38 @@ export default function Cadastrar() {
             <input type="hidden" value={1} {...register("ativo")} readOnly />
             <h3>Dados do Cliente:</h3>
             <label> Nome*  </label><br />
-            <input name="nomeCliente" placeholder="Jose da Silva" required {...register("nome")} /> <br />
+            <input name="nomeCliente" placeholder="Jose da Silva" 
+            required {...register("nome")} /> <br />
 
             <label>e-mail </label><br />
-            <input name="email" type="email" placeholder="josesilva@exemplo.com" {...register("email")} /><br />
+            <input name="email" type="email" 
+            placeholder="josesilva@exemplo.com" {...register("email")} /><br />
 
             <label>data de nascimento*</label> <br />
-            <input className={styles.inputCurto} name="dtNasc" type="date" placeholder="dd/mm/aaaa" max="2010-01-01" min="1900-01-01" required {...register("dtNascimento")} /><br />
+            <input className={styles.inputCurto} name="dtNasc" 
+            type="date" placeholder="dd/mm/aaaa" max="2010-01-01" 
+            min="1900-01-01" required {...register("dtNascimento")} /><br />
           </div>
-
-
           <div>
             <br />
             <div>
               <label>Telefone celular* <br />
-                <input className={styles.inputCurto} name="tcelular" placeholder="(00) 12345-6789" required {...register("tCelular")} minLength={11} onChange={e => setTel(e.target.value)} value={tel} autoComplete="off" /><br />
+                <input className={styles.inputCurto} name="tcelular" 
+                placeholder="(00) 12345-6789" required {...register("tCelular")} 
+                onChange={e => setTel(e.target.value)} value={tel} autoComplete="off" /><br />
               </label>
               <label>Telefone fixo<br />
-                <input className={styles.inputCurto} name="tfixo" placeholder="(00) 1234-5678" {...register("tFixo")} minLength={10} onChange={e => setTelFixo(e.target.value)} value={telFixo} autoComplete="off" /><br />
+                <input className={styles.inputCurto} name="tfixo" 
+                placeholder="(00) 1234-5678" {...register("tFixo")} 
+                onChange={e => setTelFixo(e.target.value)} value={telFixo} 
+                autoComplete="off" /><br />
               </label>
             </div>
 
             <label>CPF*</label><br />
-            <input className={styles.inputCurto} id="cpf" name="cpf" placeholder="111.222.333-44" required {...register("cpf")} minLength={14} onChange={e => setCpf(e.target.value)} value={cpf} autoComplete="off" /><br />
+            <input className={styles.inputCurto} id="cpf" name="cpf" 
+            placeholder="111.222.333-44" required {...register("cpf")} 
+            onChange={e => setCpf(e.target.value)} value={cpf} autoComplete="off" /><br />
 
             <label>gênero </label><br />
             <select name="genero" className={styles.inputCurto} {...register("genero")}>
@@ -114,19 +130,21 @@ export default function Cadastrar() {
           </div>
           <br className={styles.br} />
         </div>
-
         <hr />
-
         <div className={styles.enderecoCliente}>
           <div>
             <h3>Endereço do cliente:</h3>
-            <input type="hidden" value={1} {...register("endereco.0.ativo")} readOnly />
+            <input type="hidden" value={1} 
+            {...register("endereco.0.ativo")} readOnly />
             <label> Nome do endereço* </label><br />
-            <input name="nome" placeholder="Casa / Trabalho" {...register("endereco.0.nome")} required /> <br />
+            <input name="nome" placeholder="Casa / Trabalho" 
+            {...register("endereco.0.nome")} required /> <br />
 
             <div>
               <label> Logradouro*<br />
-                <input className={styles.inputCurto} name="logradouro" placeholder="Av. Principal" {...register("endereco.0.logradouro")} required /> <br />
+                <input className={styles.inputCurto} 
+                name="logradouro" placeholder="Av. Principal" 
+                {...register("endereco.0.logradouro")} required /> <br />
               </label>
               <label> Num* <br />
                 <input className={styles.inputMtCurto} name="numero" placeholder="201" maxLength={6} {...register("endereco.0.numero")} required /> <br />
