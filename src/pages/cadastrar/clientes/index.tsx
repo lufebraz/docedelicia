75b5a5aa0f7d1d36d5dddab7834aa801bfd8f699
lufebraz <luizbraz98@gmail.com>
2021-05-31 00:01:36 -0300

    setting tCelular required

--- src/pages/cadastrar/clientes/index.tsx
+++ src/pages/cadastrar/clientes/index.tsx
@@ -144,7 +144,7 @@ export default function Cadastrar() {
                   placeholder="(00) 12345-6789" minLength={15}
                   value={tel} {...register("tCelular")}
                   onChange={e => setTel(e.target.value)}
-                  autoComplete="off" /><br />
+                  autoComplete="off" required/><br />
               </label>
               <label>Telefone fixo<br />
                 <input className={styles.inputCurto}
